e706eb6507877323844546095f94c426411d7eee
Klai Ghassen <60801440+KlaiGhassen@users.noreply.github.com>
2024-07-18 11:22:36 +0100

    last realese

--- flutter-node-server/public/web/flutter_service_worker.js
+++ flutter-node-server/public/web/flutter_service_worker.js
@@ -40,14 +40,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3acff25b8155329f4180020a78e09f19
 "Esprit-groupe.png": "6c8385042bf6a852e450870ca8133c31",
 "favicon.png": "2704101cb06ce66e2000356a312be25c",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "1bdaee1ec046991842292408df40051e",
+"flutter_bootstrap.js": "8cabf4a85ea87298403109a4a3b14309",
 "icons/app_launcher_icon.png": "6c8385042bf6a852e450870ca8133c31",
 "icons/error_image.jpg": "bcd9cb5c4db2aa52465e54ec3c894a0a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "b77f1208c21cf1771087c507474b4faf",
 "/": "b77f1208c21cf1771087c507474b4faf",
-"main.dart.js": "ee3beb345b9a6f90cbcc15c2e7574fd6",
+"main.dart.js": "807d21a9900243db7254a77b3144fc25",
 "version.json": "cd67c000029d5451d4a13aa4975066c1"};
 // The application shell files that are downloaded before a service worker can
 // start.
